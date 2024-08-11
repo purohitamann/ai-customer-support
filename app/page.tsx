7524522b686bcad30d422c0c8f9381d718bf2634
Aman Purohit <purohiam@sheridancollege.ca>
2024-08-11 01:52:23 -0400

    Add @google/generative-ai and dotenv dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,12 @@ import ChatBox from "../components/ChatBox";
 import Container from "@mui/material/Container";
 import { Typography, Box } from "@mui/material";
 
-
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center min-h-screen py-2">
 
       <Container maxWidth="md"  >
         <Box className="flex flex-col gap-4">
-
-
           <p className="text-sm font-medium leading-none">
             AI Customer Support
           </p>
